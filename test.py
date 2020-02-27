84cf8ed7f832ae7ca7652ceec1d86c0f777c62c9
Stephen O'Flynn <git@dak.stephenoflynn.com>
2020-02-28 00:02:50 +0100

    Test initial parse

---
+++ test.py
@@ -0,0 +1,21 @@
+import datetime
+from dateutil import parser
+import json
+import sys
+
+entries = []
+
+def convert(entry):
+	start = parser.parse(entry['start'])
+	stop = parser.parse(entry['stop'])
+	delta = stop - start
+	return (start, delta.total_seconds())
+
+def totals():
+	# json.loads takes second parameter so it should be possible to convert the JSON on the fly to tuples
+	raw = json.loads(sys.stdin.read())
+	for entry in raw:
+		entries.append(convert(entry))
+
+totals()
+print(entries)
