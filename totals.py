fa6c3931896d98a525f9296a98abec365e75b6f8
Stephen O'Flynn <git@dak.stephenoflynn.com>
2020-03-01 11:16:22 +0100

    Total seconds by day

--- totals.py
+++ totals.py
@@ -1,4 +1,3 @@
-#!/usr/local/opt/python/bin/python3.7
 import datetime
 from dateutil import parser
 import json
@@ -6,17 +5,28 @@ import sys
 
 entries = []
 
-def convert(entry):
+
+def parse_entry(entry):
 	start = parser.parse(entry['start'])
 	stop = parser.parse(entry['stop'])
 	delta = stop - start
-	return (start, delta.total_seconds())
+	return start.date(), delta.total_seconds()
 
-def totals():
+
+def sum_by_day(parsed_entries):
+	day_totals = {}
+	for parsed_entry in parsed_entries:
+		key = (parsed_entry[0].strftime("%Y,%m,%d"))
+		day_totals[key] = day_totals.get(key, 0) + parsed_entry[1]
+	return day_totals
+
+
+def main():
 	# json.loads takes second parameter so it should be possible to convert the JSON on the fly to tuples
 	raw = json.loads(sys.stdin.read())
 	for entry in raw:
-		entries.append(convert(entry))
+		entries.append(parse_entry(entry))
+	print(sum_by_day(entries))
 
-totals()
-print(entries)
+
+main()
