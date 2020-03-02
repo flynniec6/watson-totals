ea8514ecdb53fce2353fd7cef7f70054a9900d4f
Stephen O'Flynn <git@dak.stephenoflynn.com>
2020-03-02 22:27:21 +0100

    Make the code leaner

--- totals.py
+++ totals.py
@@ -1,30 +1,35 @@
 import datetime
+import time
+
 from dateutil import parser
 import json
 import sys
+
 
 def parse_entry(entry):
 	start = parser.parse(entry['start'])
 	stop = parser.parse(entry['stop'])
 	delta = stop - start
-	return start.date(), delta.total_seconds()
+	return start.strftime("%Y,%m,%d"), delta.total_seconds()
 
 
-def sum_by_day(parsed_entries):
+def total_seconds_per_day(parsed_entries):
 	day_totals = {}
-	for parsed_entry in parsed_entries:
-		key = (parsed_entry[0].strftime("%Y,%m,%d"))
-		day_totals[key] = day_totals.get(key, 0) + parsed_entry[1]
+	for day, seconds in parsed_entries:
+		day_totals[day] = day_totals.get(day, 0) + seconds
 	return day_totals
 
 
+def hours_minutes(day_totals):
+	formatted_daily_totals = {}
+	for day, total in day_totals.items():
+		formatted_daily_totals[day] = time.strftime("%Hh %Mm", time.gmtime(total))
+	return formatted_daily_totals
+
+
 def main():
-	# json.loads takes second parameter so it should be possible to convert the JSON on the fly to tuples
-	entries = []
-	raw = json.loads(sys.stdin.read())
-	for entry in raw:
-		entries.append(parse_entry(entry))
-	print(sum_by_day(entries))
+	entries = json.loads(sys.stdin.read(), object_hook=parse_entry)
+	print(hours_minutes(total_seconds_per_day(entries)))
 
 
 main()
